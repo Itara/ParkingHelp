7ff086288715bbb2aff0f5e05ac6a299774c2f7b
jhpark90 <jhpark90@pharmsoft.co.kr>
2025-07-18 16:04:03 +0900

    클래스명 수정

--- ParkingHelp/DTO/RankingDTO.cs
+++ ParkingHelp/DTO/RankingDTO.cs
@@ -1,4 +1,6 @@
-﻿namespace ParkingHelp.DTO
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace ParkingHelp.DTO
 {
     public class RankingDTO
     {
@@ -13,7 +15,7 @@
         public int? HelperId { get; set; }
         public string HelperName { get; set; } = string.Empty;
         public int HelpCount { get; set; }
-        public DateTimeOffset? FirstHelpDate { get; set; }
+        
         public DateTimeOffset? LastHelpDate { get; set; }
         public List<HelpHistoryDto> RecentHelps { get; set; } = new();
     }
@@ -21,7 +23,8 @@
     public class HelpHistoryDto
     {
         public int ReqId { get; set; }
-        public DateTimeOffset? InsertDate { get; set; }
-        public int Status { get; set; }
+        public DateTimeOffset HelpDate { get; set; }
+        
+        public int RequestMemberId { get; set; }
     }
 }
