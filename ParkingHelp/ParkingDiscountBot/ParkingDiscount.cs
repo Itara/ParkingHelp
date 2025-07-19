780529699023d3e8d1fb06e97dd5fb766d8819e6
jhpark90 <jhpark90@pharmsoft.co.kr>
2025-07-19 19:48:17 +0900

    제발좀 되라

--- ParkingHelp/ParkingDiscountBot/ParkingDiscount.cs
+++ ParkingHelp/ParkingDiscountBot/ParkingDiscount.cs
@@ -27,7 +27,7 @@ namespace ParkingHelp.ParkingDiscountBot
                 using var playwright = await Playwright.CreateAsync();
                 var browser = await playwright.Chromium.LaunchAsync(new()
                 {
-                    Headless = false,
+                    Headless = true,
                     SlowMo = 50,
                     Args = new[]
                     {
